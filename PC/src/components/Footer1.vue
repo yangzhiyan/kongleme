440e7a9f3fceced76b8d8f1b1515842f606d262f
wangjine <423707803@qq.com>
2018-11-16 15:50:25 +0800

    底部

---
+++ PC/src/components/Footer1.vue
@@ -0,0 +1,350 @@
+<template>
+<div class="foot">
+    <div class="footer">
+        <div class="ft-info">
+            <dl class="ft-info-col ft-info-col1">
+                <dt class="dt-title">全球旅游消费指南 </dt>
+                <dd>覆盖全球200多个国家和地区</dd>
+                <dd><strong class="forange">100,000,000</strong>位旅行者</dd>
+                <dd><strong class="forange">920,000</strong>家国际酒店</dd>
+                <dd><strong class="forange">21,000,000</strong>条真实点评</dd>
+                <dd><strong class="forange">382,000,000</strong>次攻略下载</dd>
+                <dd><a href="" class="forange">中国旅行行业第一部“玩法”</a></dd>
+            </dl>
+            <dl class="ft-info-col ft-info-col2">
+                <dt class="dt-title">关于我们</dt>
+                <dd><a href="">关于浪了么</a>&nbsp;&nbsp;&nbsp;&nbsp;<a href="">联系我们</a></dd>
+                <dd><a href="">隐私政策</a>&nbsp;&nbsp;&nbsp;&nbsp;<a href="">商标声明</a></dd>
+                <dd><a href="">服务协议</a>&nbsp;&nbsp;&nbsp;&nbsp;<a href="">游记协议</a></dd>
+                <dd><a href="">商城平台服务</a></dd>
+                <dd><a href="">网络信息侵权通知指引</a></dd>
+                <dd><a href="">网站地图</a><a href="" class="forange">加入浪了么</a></dd>
+
+            </dl>
+            <dl class="ft-info-col ft-info-col3">
+                <dt class="dt-title">旅行服务</dt>
+                <dd>
+                    <ul class="dt-title1">
+                        <li><a href="">旅游攻略</a></li>
+                        <li><a href="">酒店预订</a></li>
+                        <li><a href="">旅游特价</a></li>
+                        <li><a href="">国际租车</a></li>
+                        <li><a href="">旅游问答</a></li>
+                        <li><a href="">旅游保险</a></li>
+                        <li><a href="">旅游指南</a></li>
+                        <li><a href="">订火车票</a></li>
+                        <li><a href="">旅游咨询</a></li>
+                        <li><a href="">APP下载</a></li>
+                        <li><a href="" class="forange">旅游商城全球商家入驻</a></li>
+
+
+                    </ul>
+                </dd>
+            </dl>
+            <dl class="ft-info-col ft-info-col4">
+                <dd>
+                    <span class="info-col4-img info-col4-img1"></span>
+                    <p>浪了么APP<br>扫码立即下载</p>
+                </dd>
+                <dd>
+                    <span class="info-col4-img info-col4-img2"></span>
+                    <p>浪了么旅游<br>订阅号</p>
+                </dd>
+                <dd>
+                    <span class="info-col4-img info-col4-img3"></span>
+                    <p>浪了么良品<br>官方服务号</p>
+                </dd>
+            </dl>
+            <dl class="ft-info-socil">
+                <dt>向崇尚自由的加勒比海盗致敬！</dt>
+                <dd>
+                    <a class="ft-socil-sina" href=""></a>
+                    <a class="ft-socil-qqt" href=""></a>
+                    <a class="ft-socil-qzone" href=""></a>
+                </dd>
+            </dl>
+        
+        </div>
+
+        <div class="ft-links">
+            <a href="" target="_blank">杨智妍</a>
+            <a href="" target="_blank">王金娥</a>
+            <a href="" target="_blank">颜继昆</a>
+            <a href="" target="_blank">周爱</a>
+            <a href="" target="_blank">马可波罗</a>
+            <a href="" target="_blank">Onlylady女人志</a>
+            <a href="" target="_blank">艺龙旅游指南</a>
+            <a href="" target="_blank">欣欣旅游网</a>
+            <a href="" target="_blank">户外运动</a>
+            <a href="" target="_blank">365音乐网</a>
+            <a href="" target="_blank">爱问共享资料</a>
+            <a href="" target="_blank">旅游网</a>
+            <a href="" target="_blank">小说网</a>
+            <a href="" target="_blank">学习啦</a>
+            <a href="" target="_blank">游多多自助游</a>
+            <a href="" target="_blank">问答</a>
+            <a href="" target="_blank">火车时刻表</a>
+            <a href="" target="_blank">驴妈妈旅游网</a>
+            <a href="" target="_blank">好逗美食网</a>
+            <a href="" target="_blank">二手车</a>
+            <a href="" target="_blank">学习啦</a>
+            <a href="" target="_blank">游多多自助游</a>
+            <a href="" target="_blank">问答</a>
+            <a href="" target="_blank">火车时刻表</a>
+            <a href="" target="_blank">驴妈妈旅游网</a>
+            <a href="" target="_blank">好逗美食网</a>
+            <a href="" target="_blank">马可波罗</a>
+            <a href="" target="_blank">Onlylady女人志</a>
+            <a href="" target="_blank">艺龙旅游指南</a>
+            <a href="" target="_blank">欣欣旅游网</a>
+            <a href="" target="_blank">户外运动</a>
+            <a href="" target="_blank">365音乐网</a>
+            <a href="" target="_blank">爱问共享资料</a>
+            <a href="" target="_blank">旅游网</a>
+            <a href="" target="_blank">小说网</a>
+            <a href="" target="_blank">学习啦</a>
+            <a href="" target="_blank">游多多自助游</a>
+            <a href="" target="_blank">艺龙旅游指南</a>
+            <a href="" target="_blank">欣欣旅游网</a>
+            <a href="" target="_blank">户外运动</a>
+            <a href="" target="_blank">365音乐网</a>
+            <a href="" target="_blank">问答</a>
+            <a href="" target="_blank">更多友情链接>></a>
+
+
+        </div>
+
+        <div class="ft-copyright">
+            <a href="" target="_blank"></a>
+            <p>
+                © 2018 LangLeMe.cn 
+                <span>京ICP备000号</span>
+                <span href="">京公网安备00000000号</span>
+                <span href="">京ICP证0000号</span>
+                <span>违法和不良信息举报电话: 00000 举报邮箱 llm@langleme.com</span>
+            </p>
+            <p>
+                网络出版服务许可证：(总)网出证(京)字第000号
+                <span>增值电信业务经营许可证：京0000</span>
+                <span>营业执照</span>
+                <span>帮助中心</span>
+                <span>马蜂窝客服：国内</span>
+                <span class="forange">8888-888-888</span>
+                <span>海外</span>
+                <span class="forange">00-0000-0000</span>
+            </p>
+        </div>
+        
+        <div class="ft-safety">
+            <a class="ft-safety-1" href=""></a>
+            <a class="ft-safety-2" href=""></a>
+            <a class="ft-safety-3" href=""></a>
+        </div>
+    </div>
+</div>
+</template>
+
+<script>
+export default {
+    name: "Footer1",
+    data() {
+        return {
+         };
+    }
+}
+</script>
+
+<style>
+*{
+    margin: 0;
+    padding: 0;
+}
+a{
+    text-decoration: none;
+}
+.foot{
+    background-color: #3c3c3c;
+    color: #c2c2c2 ;
+    line-height: 22px;
+    font-size: 12px;
+}
+.footer{
+    width: 1105px;
+    height: 558px;
+    margin: 0 auto;
+    padding-bottom: 20px;
+    /* border: 1px solid red; */
+}
+.ft-info{
+    width: 1088px;
+    height: 263px;
+    padding: 30px 8px 40px 8px;
+    border-bottom: 1px solid #575a5b;
+    /* border: 1px solid rgb(0, 255, 136); */
+}
+.ft-info-col{
+    float: left;
+}
+.ft-info-col1{
+    width: 200px;
+    margin-right: 68px;
+}
+.ft-info-col2{
+    width: 128px;
+    margin-right: 85px;
+}
+.ft-info-col3{
+    width: 135px;
+    margin-right: 80px;
+}
+.ft-info-col4 dd{
+    width: 100px;
+    height: 130px;
+    display: inline;
+    margin-left: 30px;
+    float: right;
+    text-align: center;
+}
+.info-col4-img{
+    display: inline-block;
+    width: 90px;
+    height: 90px;
+}
+.info-col4-img1{
+    background-image: url("../../static/img/ewm2.png")
+}
+.info-col4-img2{
+    background-image: url("../../static/img/ewm1.gif")
+}
+.info-col4-img3{
+    background-image: url("../../static/img/ewm1.gif")
+}
+.dt-title{
+    font-size: 14px;
+    height: 30px;
+}
+.dt-title1>li{
+    display: inline-block;
+    margin-right: 14px;
+}
+
+dd>a{
+    color: #c2c2c2 ;
+} 
+dd>a:hover{
+    color: white ;
+}
+li>a{
+    color: #c2c2c2 ;
+}
+.forange{
+    color: #ff9d00;
+}
+.ft-info-socil{
+    width: 1089px;
+    height: 100px;
+    position: relative; 
+    top: 180px; 
+}
+.ft-info-socil>dt{
+    position: absolute;
+    top: 20%;
+    left: 50%;
+    margin-left: -112px;
+    font-size: 16px;
+    height: 30px;
+}
+.ft-info-socil>dd{
+    width: 130px;
+    height: 40px;
+    position: absolute;
+    top: 56%;
+    left: 44%;
+    display: flex;
+    justify-content: space-between;
+}
+.ft-info-socil>dd>a{
+    display: inline-block;
+    width: 28px;
+    height: 28px;
+    background-image: url("../../static/img/footer1.png");
+}
+
+.ft-socil-sina{
+    background-position: 0px 0px;
+
+}
+.ft-socil-qqt{
+    background-position: -30px 0px;
+}
+.ft-socil-qzone{
+    background-position: -60px 0px;
+}
+.ft-links{
+    width: 1096px;
+    height: 66px;
+    border-top: 1px solid #575a5b;
+    border-bottom: 1px solid #575a5b;
+    padding: 17px 0 17px 8px;
+    word-wrap: break-word;
+    /* border: 1px solid beige; */
+}
+.ft-links>a{
+    color: #c2c2c2;
+    line-height: 22px;
+    padding-bottom: 20px;
+    font-size: 12px;
+    margin-right: 12px;
+}
+.ft-links>a:hover{
+    cursor: pointer;
+    color: white;
+}
+.ft-copyright{
+    width: 1084px;
+    height: 45px;
+    padding: 12px 0 12px 20px;
+    margin-top: -1px;
+    border-top: 1px solid #575a5b;
+    /* border: 1px solid orange; */
+}
+.ft-copyright>a{
+    float: left;
+    margin: 2px 20px 0 0;
+    width: 136px;
+    height: 39px;
+    background: url("../../static/img/footer1.png") no-repeat 0 -30px;
+}
+.ft-copyright p>span{
+    margin-right: 8px;
+}
+.ft-copyright p>span:hover{
+    cursor: pointer;
+    color: white;
+}
+.ft-safety{
+    width: 1105px;
+    height: 37px;
+    text-align: center;
+}
+.ft-safety>a{
+    display: inline-block;
+    margin-left: 10px;
+    width: 90px;
+    height: 30px;
+    background: url("../../static/img/footer1.png");
+}
+.ft-safety .ft-safety-1{
+    background-position: 0px -70px;
+}
+.ft-safety .ft-safety-2{
+    background-position: 0px -110px;
+}
+.ft-safety .ft-safety-3{
+    background-position: 0px -150px;
+}
+
+
+
+
+</style>
--- PC/src/router/index.js
+++ PC/src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import Router from 'vue-router';
 import IndexLunbo from '@/components/IndexLunbo'
 import Headernav from '@/components/Headernav'
+import Footer1 from '@/components/Footer1'
 
 Vue.use(Router)
 
@@ -17,6 +18,11 @@ export default new Router({
       path: '/header',
       name: 'Headernav',
       component: Headernav
+    },
+    {
+      path: '/ft',
+      name: 'Footer1',
+      component: Footer1
     }
     
   ]
